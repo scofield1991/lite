95bfb17b2c19d66e9eb4ea12fbc385431678efea
Oleksandr Teslenko <scofield1991@yandex.ru>
2015-04-21 22:26:03 +0300

    Second commit

--- lite_app/forms.py
+++ lite_app/forms.py
@@ -10,6 +10,7 @@ class UserForm(forms.ModelForm):
         fields= ('first_name','last_name', 'username', 'email', 'password')
         
 class UserProfileForm(forms.ModelForm):
+    birthday=forms.DateField(input_formats=['%Y-%m-%d'])
     class Meta:
         model = UserProfile
         fields=('phone_number', 'picture', 'birthday')
--- lite_app/views.py
+++ lite_app/views.py
@@ -1,7 +1,69 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from lite_app.forms import UserForm, UserProfileForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def index(request):
     context_dict = {'boldmessage': 'Hi there!'}
     return render(request, 'lite_app/index.html', context_dict)
+
+
+def register(request):
+
+    registered=False
+
+    if request.method=='POST':
+        user_form=UserForm(data=request.POST)
+        profile_form=UserProfileForm(data=request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user=user_form.save()
+            user.set_password(user.password)
+            user.save()
+
+            profile=profile_form.save(commit=False)
+            profile.user=user
+
+            if 'picture' in request.FILES:
+                profile.picture=request.FILES['picture']
+
+            profile.save()
+
+            registered=True
+
+        else:
+            print (user_form.errors, profile_form.errors)
+
+    else:
+        user_form= UserForm()
+        profile_form= UserProfileForm()
+
+    return render(request, 'lite_app/register.html',
+        {'user_form': user_form, 'profile_form':profile_form, 'registered': registered})
+
+
+def user_login(request):
+    if request.method=='POST':
+        username= request.POST.get('username')
+        password= request.POST.get('password')
+
+        user=authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('/lite/')
+            else:
+                return HttpResponse("Your account is inactive!")
+        else:
+            print ("Invalid login details: {0}, {1}". format(username, password))
+            return HttpResponse("Invalid login details supplied")
+    else:
+        return render(request, 'lite_app/login.html', {})
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect('/lite/')
